1d293635a662f947681d62bbe7c4e687ea016589
P.Bondarovich <p.bondarovich@gmail.com>
2018-02-27 17:32:17 +0700

    muliple upload images to gallery

--- Admin/GalleryTable.php
+++ Admin/GalleryTable.php
@@ -2,21 +2,40 @@
 
 namespace Creonit\MediaBundle\Admin;
 
-use Creonit\MediaBundle\Model\GalleryItemQuery;
 use Creonit\AdminBundle\Component\Request\ComponentRequest;
 use Creonit\AdminBundle\Component\Response\ComponentResponse;
 use Creonit\AdminBundle\Component\Scope\Scope;
 use Creonit\AdminBundle\Component\TableComponent;
-use Propel\Runtime\ActiveQuery\ModelCriteria;
+use Creonit\MediaBundle\Model\GalleryItemQuery;
+use Creonit\MediaBundle\Model\GalleryQuery;
+use Creonit\MediaBundle\Model\GalleryItem;
 
 class GalleryTable extends TableComponent
 {
 
 
     /**
+     * @action uploadImages(query){
+     *   var $form = $('<form><input type="file" name="files" multiple></form>');
+     *   var $file = $form.find('input');
+     *   var $button = this.node.find('button[data-name="uploadImages"]')
+     *   var $buttonIcon = $button.find('.icon')
+     *
+     *   $file.on('change', function(){
+     *     $button.prop('disabled', true);
+     *     $buttonIcon.removeClass('fa-image').addClass('fa-spin fa-spinner');
+     *     this.request('upload_images', query, {files: $file[0].files}, function(){
+     *       $buttonIcon.addClass('fa-image').removeClass('fa-spin fa-spinner');
+     *       $button.prop('disabled', false);
+     *     }.bind(this));
+     *     this.loadData();
+     *   }.bind(this));
+     *
+     *   $file.click();
+     * }
      *
      * @header
-     * {{ button('Добавить изображение', {icon: 'image', size: 'sm', type: 'success'}) | open('Media.GalleryImageEditor', _query) }}
+     * {{ button('Добавить изображения', {icon: 'image', size: 'sm', type: 'success'}) | action('uploadImages', _query) }}
      * {{ button('Добавить видео', {icon: 'youtube-play', size: 'sm', type: 'success'}) | open('Media.GalleryVideoEditor', _query) }}
      *
      * @cols Изображение / Видео, .
@@ -41,6 +60,7 @@ class GalleryTable extends TableComponent
      */
     public function schema()
     {
+        $this->addHandler('upload_images', [$this, 'uploadImages']);
     }
 
     /**
@@ -55,7 +75,29 @@ class GalleryTable extends TableComponent
     protected function filter(ComponentRequest $request, ComponentResponse $response, $query, Scope $scope, $relation, $relationValue, $level)
     {
         $query->filterByGalleryId($request->query->get('gallery_id'));
-        
+
+    }
+
+    public function uploadImages(ComponentRequest $request, ComponentResponse $response)
+    {
+        if (!GalleryQuery::create()->findPk($request->query->get('gallery_id'))) {
+            $response->error('Галерея не найдена');
+        }
+
+        if($files = $request->data->get('files')){
+            $imageField = $this->createField('image_id', [], 'image');
+
+            foreach ($files as $file){
+                $file = ['file' => $file, 'delete' => false];
+                $entity = new GalleryItem();
+                $entity->setGalleryId($request->query->get('gallery_id'));
+                if($imageField->validate($file)->count()){
+                    continue;
+                }
+                $imageField->save($entity, $file);
+                $entity->save();
+            }
+        }
     }
 
 
